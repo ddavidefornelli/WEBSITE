5967d9da923edb655774adb5f1764dac9d4d380e
ddavidefornelli <fornelli.dv@gmail.com>
2025-12-10 17:29:32 +0100

    ciao

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,11 +1,12 @@
 import './footer.css';
+
 export default class CustomFooter extends HTMLElement {
   connectedCallback() {
     this.loadHTML();
   }
 
   async loadHTML() {
-    const res = await fetch('./footer.html');
+    const res = await fetch('/components/footer/footer.html');
     const data = await res.text();
     this.innerHTML = data;
   }
